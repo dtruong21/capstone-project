abfb1edf65d4d16f9ab216fff51f9cf6759f739b
cminhtruong <cminh.truong@gmail.com>
2018-07-27 12:02:40 +0200

    add detail activity to display all dalker info. Not include business logic

---
+++ app/src/main/java/dalker/cmtruong/com/app/view/DetailDalkerActivity.java
@@ -0,0 +1,15 @@
+package dalker.cmtruong.com.app.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dalker.cmtruong.com.app.R;
+
+public class DetailDalkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_dalker);
+    }
+}
